b455747feb9bfd37a6baf16ce403fd9983ff1545
Arash K <arashkashi@gmail.com>
2021-01-25 12:40:53 +0330

    login working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
