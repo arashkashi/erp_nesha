512bc9e04ff3532c6cd12acad84e224bcd9dbe08
Arash K <arashkashi@gmail.com>
2021-02-04 19:03:03 +0330

    added validation for json

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
+class UserRolesValidator < ActiveModel::Validator
+ 	def validate(record)
+ 		JSON.parse(record.roles)
+  		rescue JSON::ParserError => e
+    	record.errors.add :roles, "Not valid JSON string"
+  	end
+end
+
+
 class User < ApplicationRecord
   has_secure_password
 
+  validates_with UserRolesValidator
   validates :email, presence: true, uniqueness: true
 end
