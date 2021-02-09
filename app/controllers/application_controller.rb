8019e6cbe41efcf414dd900103cdd755b7936966
Arash K <arashkashi@gmail.com>
2021-02-09 16:57:24 +0330

    login working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+	before_action :require_login
+
 	helper_method :current_user
 	helper_method :logged_in?
 
@@ -10,4 +12,13 @@ class ApplicationController < ActionController::Base
 	def logged_in?   
     	!current_user.nil?
 	end
+
+	private
+
+  	def require_login
+    	unless logged_in?
+      		flash[:error] = "You must be logged in to access this section"
+      		redirect_to root_path
+    	end
+  	end
 end
