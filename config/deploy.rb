fd5579cf48186043774db1d7d972b18c00eea659
Arash K <arashkashi@gmail.com>
2021-02-15 22:09:55 +0330

    changed to the correct deploy folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:arashkashi/erp_nesha.git"
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "~/erp_nesha"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "134.122.121.33", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "138.68.64.182", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
