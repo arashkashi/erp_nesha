a32c2dea1a179f0d1a18dbff6a55347a3c2c0b84
Arash K <arashkashi@gmail.com>
2021-02-17 19:47:27 +0330

    localizations

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.i18n.default_locale = :fa
+
     # Make the id as UUID and not incremental
     config.generators do |g|
         g.orm :active_record, primary_key_type: :uuid
