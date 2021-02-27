0bd9bf66b21ce17ac4b11fb696f2f33bae3f8e20
Arash K <arashkashi@gmail.com>
2021-02-27 22:54:55 +0330

    some localization

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,6 +1,14 @@
 class StoresController < ApplicationController
   def products
   	@pipe_types = PipeType.all
+
+    temp = 0
+  	@pipe_types.each do |p_type|
+
+  		temp = temp + (PipeStoreInput.sum_amount_by_pipe_type(p_type) - PipeStoreOutput.sum_amount_by_pipe_type(p_type))*p_type.weight
+  	end
+
+  	@total_w = temp
   end
 
   def raw_materials
