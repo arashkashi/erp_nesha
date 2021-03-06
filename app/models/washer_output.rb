f982183b09650e01d3d9a187f7c0088f92e5928b
Arash K <arashkashi@gmail.com>
2021-03-06 10:57:25 +0330

    fixed the washer stuff

--- app/models/washer_output.rb
+++ app/models/washer_output.rb
@@ -1,5 +1,19 @@
+class WasherOutputTypeValidator < ActiveModel::Validator
+ 	def validate(record)
+
+ 		if record.order_id == "" 
+ 			record.order_id = nil
+ 		end	 
+  	end
+end
+
+
 class WasherOutput < ApplicationRecord
   belongs_to :washer_type
-  belongs_to :order
+  belongs_to :order, :optional => true
   belongs_to :user
+
+  validates_with WasherOutputTypeValidator
+
+  validates :order_id, presence: false
 end
