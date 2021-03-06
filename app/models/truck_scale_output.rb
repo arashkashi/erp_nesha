cbb9d095e51b36b7ce3b244773326a4c7540c6df
Arash K <arashkashi@gmail.com>
2021-03-06 16:56:51 +0330

    fixed truck scale ids

--- app/models/truck_scale_output.rb
+++ app/models/truck_scale_output.rb
@@ -1,5 +1,18 @@
+class TruckScaleOutputValidator < ActiveModel::Validator
+ 	def validate(record)
+
+ 		if record.order_id == "" 
+ 			record.order_id = nil
+ 		end	 
+  	end
+end
+
 class TruckScaleOutput < ApplicationRecord
-  belongs_to :order
+  belongs_to :order, :optional => true
   belongs_to :user
+
+  validates_with TruckScaleOutputValidator
+
+  validates :order_id, presence: false
 
 end
