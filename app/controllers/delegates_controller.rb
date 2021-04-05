b81ee780cb564667b7d7f3913bcf2b51737b3fde
Arash K <arashkashi@gmail.com>
2021-04-05 09:58:28 +0430

    sort based on size

--- app/controllers/delegates_controller.rb
+++ app/controllers/delegates_controller.rb
@@ -3,6 +3,6 @@ class DelegatesController < ApplicationController
   skip_before_action :application_before_action
 
   def pipe_store
-  	@pipe_types = PipeType.all.order(:name)
+  	@pipe_types = PipeType.all.order(:size)
   end
 end
